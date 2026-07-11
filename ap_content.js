2cd38054b4217764100d46f7788400186c146208
Bryce Schultz <bryceschultz@live.com>
2026-07-10 18:17:26 -0600

    removed version specific classes so this works for updates (possibly)

--- ap_content.js
+++ ap_content.js
@@ -5,45 +5,28 @@
 */
 
 function getMetadataDiv() {
-    const musicPlayer = document.getElementsByTagName('amp-lcd')[0].shadowRoot;
-    const metaData = musicPlayer.querySelector('amp-lcd-metadata .lcd-meta');
-    return metaData;
+    const musicPlayer = document.getElementsByClassName('chrome-player')[0];
+    return musicPlayer;
 }
 
 function getTitleMetadataDiv() {
     const titleChild = 0;
-    return getMetadataDiv().children[titleChild];
+    return getMetadataDiv().getElementsByClassName('marquee-line__fragment')[titleChild]
 }
 
 function getArtistMetadataDiv() {
-    const artistChild = 1;
-    return getMetadataDiv().children[artistChild];
+    const artistChild = 2;
+    return getMetadataDiv().getElementsByClassName('marquee-line__fragment')[artistChild];
 }
 
 // Parse the song title from the page
 function getSongTitle() {
     const titleMetadataDiv = getTitleMetadataDiv();
-
-    const songTitleQuery = 'span.lcd-meta-line__fragment';
-    let songTitleDiv = titleMetadataDiv.querySelector(songTitleQuery);
-    if (!songTitleDiv) return "";
-
-    return songTitleDiv.innerText;
+    return titleMetadataDiv.textContent;
 }
 
 // Parse the artist from the page
 function getArtist() {
     const artistMetadataDiv = getArtistMetadataDiv();
-    
-    const artistQuery1 = 'button.lcd-meta-line__fragment';
-    let artistDiv = artistMetadataDiv.querySelector(artistQuery1);
-    if (!artistDiv) 
-    {
-        const artistQuery2 = 'span.lcd-meta-line__fragment';
-        artistDiv = artistMetadataDiv.querySelector(artistQuery2);
-    }
-
-    if (!artistDiv) return "";
-
-    return artistDiv.innerText;
+    return artistMetadataDiv.textContent;
 }
